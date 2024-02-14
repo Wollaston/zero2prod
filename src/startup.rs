326f8090f01bc6ee404633cce792b8d092be2800
Wollaston <mattflynn1997@gmail.com>
2024-02-14 17:52:11 -0500

    Add telemetry

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,7 @@
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap connection in a smart pointer
@@ -9,7 +10,8 @@ pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std
     // Capture 'connection' from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
-            .route("health-check", web::get().to(crate::routes::health_check))
+            .wrap(TracingLogger::default())
+            .route("/health-check", web::get().to(crate::routes::health_check))
             .route("/subscriptions", web::post().to(crate::routes::subscribe))
             // Get a pointer copy and attach it to the app state
             .app_data(db_pool.clone())
